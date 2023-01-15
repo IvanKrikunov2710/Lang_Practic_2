c04a0295fd89beb46bf43779c32f86ad06a64c0d
Ivan <Sernaiv@yandex.ru>
2023-01-15 22:40:03 +0300

    случайное число и сравнение чисел

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Random random = new Random();
+int number = 0;
+number = random.Next(10,100);
+Console.WriteLine(number);
+// 56 % 10 = 6
+// 56 / 10 = 5
+int a = number / 10;
+int b = number % 10;
+if (a > b)
+Console.WriteLine("a > b");
+else if (b > a)
+Console.WriteLine("b > a");
+else if (b == a)
+Console.WriteLine("b == a");
