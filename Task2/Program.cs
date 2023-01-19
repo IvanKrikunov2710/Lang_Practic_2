5441d163f2f836eddcaaea0d1f6641f48e58a2da
Ivan <Sernaiv@yandex.ru>
2023-01-19 19:19:50 +0300

    случайное трехзначное число в двухзначное из первой и последней цифры

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа выбирает рандомное трехзначное число,
+// в ответ выводит двузначное число, состоящее из первой и 
+// последней цифры этого числа
+
+Random random = new Random();
+int number = 0;
+number = random.Next(99,1000);
+Console.WriteLine(number);
+int a = number / 100;
+int b = number % 10;
+Console.Write(a);
+Console.Write(b);
